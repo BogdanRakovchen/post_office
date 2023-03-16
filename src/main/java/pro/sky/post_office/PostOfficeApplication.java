cb2d202f142739347386a4d1ba0cecaa59566383
Bogdan <bogdan-dvzd.rzd@mail.ru>
2023-03-16 17:14:38 +1000

    initial commit

---
+++ src/main/java/pro/sky/post_office/PostOfficeApplication.java
@@ -0,0 +1,16 @@
+package pro.sky.post_office;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+public class PostOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostOfficeApplication.class, args);
+	}
+
+}
